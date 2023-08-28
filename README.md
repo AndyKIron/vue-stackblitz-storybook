b560969bfe71723d4d1c115d1ee5e946cf09be05
AndyKIron <andy.kono@gmail.com>
2023-08-28 09:10:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-stackblitz-storybook
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-qzqx4c)
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <hr />
+    <Typography variant="title">Title</Typography>
+    <hr />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Heading from '@ironsource/shared-ui/heading';
+import Typography from '~@ironsource/shared-ui/components/v4/Typography.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Typography,
+  },
+};
+</script>
+
+<style>
+@import './node_modules/@ironsource/shared-ui/styles/levelplay/fonts.css';
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
